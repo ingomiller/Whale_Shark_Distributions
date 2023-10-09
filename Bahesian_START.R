70298c4661d3a1e7209bd1266215e84336ae228c
Ingo Miller <jc563815@ad.jcu.edu.au>
2023-10-09 19:52:02 +1000

    implementoing rstan and sdmTMB packages

---
+++ Bahesian_START.R
@@ -0,0 +1,6 @@
+
+
+# run the next line if you already have rstan installed
+# remove.packages(c("StanHeaders", "rstan"))
+
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
